8c75fa409d1b1b3ffdb154d61dbe16882f626bc7
dylee <egni1227@naver.com>
2025-04-17 16:30:14 +0900

    데이터 전달: (2)콘솔출력, DeptVO.java의 get set 메소드(전역)를 통한 데이터 전달

--- src/main/java/egovframework/example/sample/service/DeptVO.java
+++ src/main/java/egovframework/example/sample/service/DeptVO.java
@@ -3,6 +3,7 @@ package egovframework.example.sample.service;
 public class DeptVO {
 	
 	//객체지향: 캡슐화(접근지정자 세팅)
+	//변수명을 HTML name과 똑같이 지정해줘야 인식이 됨
 	private String deptno;
 	private String dname;
 	private String loc;
@@ -26,5 +27,4 @@ public class DeptVO {
 		this.loc = loc;
 	}
 	
-
 }
--- src/main/java/egovframework/example/sample/web/DeptController.java
+++ src/main/java/egovframework/example/sample/web/DeptController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import egovframework.example.sample.service.DeptVO;
+
 // @ : Annotation(시스템 주석, 기능 부여)
 @Controller
 public class DeptController {
@@ -13,11 +15,11 @@ public class DeptController {
 		return "dept/deptWrite";
 	}
 	@RequestMapping(value="deptWriteSave.do")
-	public String deptWriteSave(String deptno, String dname, String loc ) {
+	public String deptWriteSave(DeptVO vo) {
 		
-		System.out.println("부서번호: " + deptno);
-		System.out.println("부서이름: " + dname);
-		System.out.println("부서위치: " + loc);
+		System.out.println("부서번호: " + vo.getDeptno());
+		System.out.println("부서이름: " + vo.getDname());
+		System.out.println("부서위치: " + vo.getLoc());
 		
 		return "";
 	}
