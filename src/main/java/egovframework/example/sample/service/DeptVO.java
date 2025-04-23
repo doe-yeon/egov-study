73cc08b3e56be1b541570dbc7638713eceedc52a
dylee <egni1227@naver.com>
2025-04-23 15:27:00 +0900

    오라클 설정: pom.xml

--- src/main/java/egovframework/example/sample/service/DeptVO.java
+++ src/main/java/egovframework/example/sample/service/DeptVO.java
@@ -4,11 +4,12 @@ public class DeptVO {
 	
 	//객체지향: 캡슐화(접근지정자 세팅)
 	//변수명을 HTML name과 똑같이 지정해줘야 인식이 됨
-	private String deptno;
+	//VO는 데이터를 캡슐화하는 객체로, 특정 데이터를 표현하는 역할을 합니다. 주로 **불변성(immutable)**을 가지며, 값을 변경할 수 없고, 생성될 때만 값이 결정됩니다.
+	private String deptno; //html(deptWrite.jsp)의 name 속성값과 같아야함
 	private String dname;
 	private String loc;
 	
-	public String getDeptno() {
+	public String getDeptno() { //전역 메소드 설정 
 		return deptno;
 	}
 	public void setDeptno(String deptno) {
@@ -26,5 +27,5 @@ public class DeptVO {
 	public void setLoc(String loc) {
 		this.loc = loc;
 	}
-	
+	 
 }
--- src/main/java/egovframework/example/sample/web/DeptController.java
+++ src/main/java/egovframework/example/sample/web/DeptController.java
@@ -7,17 +7,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import egovframework.example.sample.service.DeptVO;
 
 // @ : Annotation(시스템 주석, 기능 부여)
-@Controller
+@Controller //웹주소 인식 기능
 public class DeptController {
 	
-	@RequestMapping(value="/deptWrite.do")
-	public String deptWrite() {
-		return "dept/deptWrite";
+	@RequestMapping(value="/deptWrite.do") //직접 웹주소 적는 설정
+	public String deptWrite() { //위 주소에 대한 실행은 메서드가 담당
+		return "dept/deptWrite"; //jsp파일 경로(화면 뜰 수 있게)
 	}
-	@RequestMapping(value="deptWriteSave.do")
-	public String deptWriteSave(DeptVO vo) {
+	
+	@RequestMapping(value="/deptWriteSave.do") //저장버튼 클릭시 (form태그의 action속성에 지정되어있음)
+	public String deptWriteSave(DeptVO vo) { //데이터가 매개변수로 들어옴 (DeptVO.java 클래스)
 		
-		System.out.println("부서번호: " + vo.getDeptno());
+		System.out.println("부서번호: " + vo.getDeptno()); //DeptVO.java클래스의 전역메서트 호출
 		System.out.println("부서이름: " + vo.getDname());
 		System.out.println("부서위치: " + vo.getLoc());
 		
